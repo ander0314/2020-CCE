1605a9fc47a6f5a76598e759fa08a9a144df8560
ander0314 <79676773+ander0314@users.noreply.github.com>
2021-03-26 15:21:28 +0800

    Add files via upload

---
+++ 09160225.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+{
+
+        char line [5][10]="decline","proper","majority"}
+
+        for (int i=0; i<5; i++){
+            printf("%s,\n", line[i]);
+        }
+        }
